b1e527cbd17baa60413985a11737d9aff9590d4c
nathansabinogm <nathansabinogamer@gmail.com>
2025-01-23 19:40:25 -0300

    Update create.js

--- create.js
+++ create.js
@@ -1,6 +1,7 @@
 
 var btn = document.querySelector("button")
 var btn2 = document.querySelector("button")
+var btn3 = document.querySelector("button")
 
 btn.onclick = (e) => {
     e.preventDefault()
@@ -19,3 +20,13 @@ btn.onclick = (e) => {
     listItem.innerText = 'novo item +'
     lista.appendChild(listItem)
 }
+
+   btn3.onclick = (e) => {
+    e.preventDefault()
+    
+    const lista = document.querySelector("#lista");
+    const listItem = document.querySelector("li");
+    listItem.innerText = 'novo item +'
+    lista.appendChild(listItem)
+}
+
